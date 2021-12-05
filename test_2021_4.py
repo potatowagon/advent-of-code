ffa330333d2b02f20a8a1ea2f9b36dd7b58afa6b
Sherry Wong <potatowagon@fb.com>
2021-12-05 11:50:36 +0800

    d4 p2

--- test_2021_4.py
+++ test_2021_4.py
@@ -9,6 +9,7 @@ class HashedBoard:
     def __init__(self, board):
         self.r_marked2count = {}
         self.c_marked2count = {}
+        self.last_marked = None
         self.val2rc = collections.defaultdict(list)
         for r in range(len(board)):
             for c in range(len(board[0])):
@@ -20,6 +21,7 @@ class HashedBoard:
             self.r_marked2count[r] = self.r_marked2count.get(r, 0) + 1
             self.c_marked2count[c] = self.c_marked2count.get(c, 0) + 1
         del self.val2rc[val]
+        self.last_marked = val
 
     def is_win(self):
         for k, v in self.r_marked2count.items():
@@ -44,6 +46,22 @@ def soln(rolled, boards) -> int:
                 return v * b.get_unmarked_vals_sum()
     return -1
 
+def soln2(rolled, boards) -> int:
+    hashed_boards = [HashedBoard(b) for b in boards]
+    last_win = None
+
+    for v in rolled:
+        leftovers = []
+        for b in hashed_boards:
+            b.mark(v)
+            if b.is_win():
+                last_win = b
+            else:
+                leftovers.append(b)
+        hashed_boards = leftovers
+
+    return last_win.get_unmarked_vals_sum() * last_win.last_marked if last_win else -1
+
 
 def _parse(input):
     input = [list(y) for x, y in itertools.groupby(input, lambda z: z == "") if not x]
@@ -59,9 +77,11 @@ def test_sample():
     sample_input = utils.read_str_input("2021_4_s.txt")
     rolled, boards = _parse(sample_input)
     assert soln(rolled, boards) == 4512
+    assert soln2(rolled, boards) == 1924
 
 
 def test_real():
     my_input = utils.read_str_input("2021_4.txt")
     rolled, boards = _parse(my_input)
     assert soln(rolled, boards) == 44088
+    assert soln2(rolled, boards) == 23670
